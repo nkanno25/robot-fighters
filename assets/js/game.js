bc95dc9d59c4542f8f7d99a56ea5d74a84bda8eb
Luka <nickolas.kanno@gmail.com>
2022-04-18 14:40:35 -0700

    3.2.6

--- assets/js/game.js
+++ assets/js/game.js
@@ -6,11 +6,17 @@ var playerMoney = 10;
 //log multiple values at once like this
 console.log(playerName, playerAttack, playerHealth)
 
-var enemyName = "Roberto";
+var enemyNames = ["Roberto", "Amy Android", "Robo Trumble"];
 var enemyHealth = 50;
 var enemyAttack = 12;
 
-var fight = function() {
+console.log(enemyNames); 
+console.log(enemyNames[0]);
+console.log(enemyNames[1]);
+console.log(enemyNames[2]);
+console.log(enemyNames.length);
+
+var fight = function(enemyName) {
 
     // Alert players that they are starting the round
     window.alert("Welcome to Robot Fighters!");
@@ -70,4 +76,6 @@ var fight = function() {
 }
 };
 
-fight();
+for(var i = 0; i < enemyNames.length; i++) {
+        fight(enemyNames[i]);
+}
